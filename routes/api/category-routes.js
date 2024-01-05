7545c8a1c8447b302219cb65be2fd2812a31dffd
Xenodius <xenodius@hotmail.com>
2024-01-05 13:14:59 -0400

    added CRUD methods to the tag-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -47,13 +47,20 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
-    const categoryData = await Category.findByPk(req.params.id);
+    const categoryData = await Tag.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    });
     if (!categoryData) {
       res.status(404).json({ message: 'No category found with this id!'});
       return;
     }
-    await categoryData.update(req.body);
-    res.status(200).json(categoryData);
+    res.status(200).json({ message: 'Updated category name'});
   } catch (error) {
     res.status(500).json(error);
   }
